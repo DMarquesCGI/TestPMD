ab0fb60f0df77d52bafaabdcfff35c2558ad413b
Daniel Marques <DMarques@elxtemp.internal>
2018-09-25 10:30:33 +0000

    Adding new .java

---
+++ src/main/java/com/tooltest/App1.java
@@ -0,0 +1,15 @@
+package com.toolstest;
+
+/**
+ * Hello world!
+ */
+public class App {
+	
+	//private String name;
+    public static void main(String[] args) {
+        int i = 1;
+		int o = 2;
+		int u = i+o;
+		System.out.println(u);
+    }
+}
---
+++ src/main/java/com/tooltest/App2.java
@@ -0,0 +1,12 @@
+package com.toolstest;
+
+/**
+ * Hello world!
+ */
+public class App {
+	
+	//private String name;
+    public static void main(String[] args) {
+        System.out.println("Hello Planet!");
+    }
+}
